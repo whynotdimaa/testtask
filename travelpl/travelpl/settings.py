59030447255c2a98cf8d30756f8ece236a49df71
whynotdimaa <dmytro.zhuk1211@gmail.com>
2026-03-27 16:13:10 +0200

    f

--- travelpl/travelpl/settings.py
+++ travelpl/travelpl/settings.py
@@ -51,6 +51,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+}
+
 ROOT_URLCONF = 'travelpl.urls'
 
 TEMPLATES = [
